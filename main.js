a17b6eb666420130c9f5bd84cf77946f2d094613
mukanovaamina <125591558+mukanovaamina@users.noreply.github.com>
2024-01-07 14:05:09 +0600

    Update and rename guzz.js to main.js

--- main.js
+++ main.js
@@ -1,20 +1,6 @@
 function showSuccessMessage() {
   alert("You have successfully subscribed to our newsletter!");
 }
-function sendMessage() {
-  var messageInput = document.getElementById("message-input");
-  var messageText = messageInput.value.trim();
-
-  if (messageText !== "") {
-    var chatMessages = document.getElementById("chat-messages");
-    var newMessage = document.createElement("div");
-    newMessage.className = "message";
-    newMessage.textContent = "You: " + messageText;
-    chatMessages.appendChild(newMessage);
-
-    messageInput.value = "";
-  }
-}
 // Get the button
 var mybutton = document.getElementById("scrollToTopBtn");
 
